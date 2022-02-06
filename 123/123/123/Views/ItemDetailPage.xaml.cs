1fd3d73f21d6297377af31d9a04b3ba2ca2e421f
OstwaldAnna <asyakynz@gmail.com>
2022-02-06 19:51:44 +0300

    мобильное приложение

---
+++ 123/123/123/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using _123.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace _123.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ 123/123/123/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using _123.Models;
+using _123.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _123.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
