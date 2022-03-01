1cedbec13333c0953c64953f965c22188e3a4c7f
OstwaldAnna <asyakynz@gmail.com>
2022-03-02 00:05:25 +0300

    reg

--- TestProg/Pages/RegPage.xaml.cs
+++ TestProg/Pages/RegPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestProg.Bd;
+using TestProg.Cl;
 
 namespace TestProg.Pages
 {
@@ -23,6 +25,23 @@ namespace TestProg.Pages
         public RegPage()
         {
             InitializeComponent();
+        }
+
+        private void BtnAuth_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User()
+            {
+                Login = (string)NameTextBox.SelectedText,
+                Password = (string)PasswordBox.Password,
+            };
+            ODBClass.entities.User.Add(user);
+            ODBClass.entities.SaveChanges();
+            MessageBox.Show("Поздравляю с успешной регистрацией");
+            GlavnWin win = new GlavnWin();
+            win.Show();
+
+           
+
         }
     }
 }
